7685674cc32f37135baf18a00204bd3ee3ad7956
Terrokot <egor.tereshonok@gmail.com>
2020-01-22 19:40:38 +0300

    Implementing parsing logic

--- vkClient/vkClient/Auth/FeedViewController/FeedViewController.swift
+++ vkClient/vkClient/Auth/FeedViewController/FeedViewController.swift
@@ -10,32 +10,38 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    private let networkService: Networking = NetworkService()
-
+    //private let networkService: Networking = NetworkService()
+    private var fether: DataFetcher = NetworkDataFetcher(networking: NetworkService())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.3725763559, green: 0.679697752, blue: 0.9031428695, alpha: 1)
+        fether.getFeed { (feedResponse) in
+            guard let feedResponse = feedResponse else { return }
+            feedResponse.items.map { (feedItem) in
+                print(feedItem.date)
+            }
+        }
+            
+        /*
         let params = ["filters": "post, photo"]
         networkService.request(path: API.newsFeed, params: params) {(data, error) in
             if let error = error {
                 print("Error recived requesting data: \(error.localizedDescription)")
             }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
             guard let data = data else { return }
-            let json = try? JSONSerialization.jsonObject(with: data, options: [])
-            print("json: \(json)")
+           // let json = try? JSONSerialization.jsonObject(with: data, options: [])
+           // print("json: \(json)")
+            let response = try? decoder.decode(FeedResponseWrapped.self, from: data)
+           // print(response ?? " ")
+            response?.response.items.map({(feedItem) in
+                print(feedItem.text)
+            })
         }
-        // Do any additional setup after loading the view.
-    }
-    
+ */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
---
+++ vkClient/vkClient/Services/Models/FeedResponse.swift
@@ -0,0 +1,32 @@
+//
+//  FeedResponse.swift
+//  vkClient
+//
+//  Created by Egor Tereshonok on 1/22/20.
+//  Copyright © 2020 Egor Tereshonok. All rights reserved.
+//
+
+import Foundation
+
+struct FeedResponseWrapped: Decodable {
+    let response: FeedResponse
+}
+
+struct FeedResponse: Decodable {
+    var items: [FeedItem]
+}
+
+struct FeedItem: Decodable {
+    let sourceId: Int
+    let postId: Int
+    let text: String?
+    let date: Double
+    let comments: CountableItem?
+    let likes: CountableItem?
+    let reposts: CountableItem?
+    let views: CountableItem?
+}
+
+struct CountableItem: Decodable {
+    let count: Int
+}
