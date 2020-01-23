876414057bc304bf10bea321309bb50cd8446514
Terrokot <egor.tereshonok@gmail.com>
2020-01-23 16:13:37 +0300

    add date and groups names

---
+++ vkClient/vkClient/Newfeed/Cell/NewsfeedCell.swift
@@ -0,0 +1,61 @@
+//
+//  NewsfeedCell.swift
+//  vkClient
+//
+//  Created by Egor Tereshonok on 1/23/20.
+//  Copyright © 2020 Egor Tereshonok. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol FeedCellViewModel {
+    var iconUrlString: String {  get }
+    var name: String { get }
+    var date: String { get }
+    var text: String? { get }
+    var likes: String? { get }
+    var comments: String? { get }
+    var shares: String? { get }
+    var views: String? { get }
+}
+
+class NewsfeedCell: UITableViewCell {
+    
+    
+    static let reuseId = "NewsfeedCell"
+    
+    //topView
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    //mediumView
+    
+    @IBOutlet weak var postLabel: UILabel!
+    
+    //downView
+    
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var sharesLabel: UILabel!
+    @IBOutlet weak var viewsLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+   
+    func set(viewModel: FeedCellViewModel) {
+        nameLabel.text = viewModel.name
+        dataLabel.text = viewModel.date
+        postLabel.text = viewModel.text
+        likesLabel.text = viewModel.likes
+        commentsLabel.text = viewModel.comments
+        sharesLabel.text = viewModel.shares
+        viewsLabel.text = viewModel.views
+    }
+    
+}
+
--- vkClient/vkClient/Newfeed/NewsfeedPresenter.swift
+++ vkClient/vkClient/Newfeed/NewsfeedPresenter.swift
@@ -7,34 +7,55 @@
 //
 
 import UIKit
-
+import Foundation
 protocol NewsfeedPresentationLogic {
   func presentData(response: Newsfeed.Model.Response.ResponseType)
 }
 
 class NewsfeedPresenter: NewsfeedPresentationLogic {
   weak var viewController: NewsfeedDisplayLogic?
+    
+    
   
   func presentData(response: Newsfeed.Model.Response.ResponseType) {
   
     switch response {
     case .presentNewsfeed(let feed):
         let cells = feed.items.map { (feedItem) in
-            cellViewModel(from: feedItem)
+            cellViewModel(from: feedItem, profiles: feed.profiles, groups: feed.groups)
         }
         let feedViewModel = FeedViewModel.init(cells: cells)
         viewController?.displayData(viewModel: Newsfeed.Model.ViewModel.ViewModelData.displayNewsfeed(feedViewModel: feedViewModel))
     }
   }
     
-    private func cellViewModel(from feedItem: FeedItem) -> FeedViewModel.Cell {
-        return FeedViewModel.Cell.init(iconUrlString: "",
-                                       name: "future name",
-                                       date: "future date",
+    private func cellViewModel(from feedItem: FeedItem, profiles: [Profile], groups: [Group]) -> FeedViewModel.Cell {
+        
+        let profile = self.profile(for: feedItem.sourceId, profiles: profiles, groups: groups)
+        
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "ru_RU")
+            dateFormatter.dateFormat = "d MMM 'в' HH:mm"
+        
+        let date = Date(timeIntervalSince1970: feedItem.date)
+        let dateTitle = dateFormatter.string(from: date)
+        
+        return FeedViewModel.Cell.init(iconUrlString: profile.photo,
+                                       name: profile.name,
+                                       date: dateTitle,
                                        text: feedItem.text,
                                        likes: String(feedItem.likes?.count ?? 0),
                                        comments: String(feedItem.comments?.count ?? 0),
                                        shares: String(feedItem.reposts?.count ?? 0),
                                        views: String(feedItem.views?.count ?? 0))
+    }
+    
+    private func profile(for sourceId: Int, profiles: [Profile], groups: [Group]) -> ProfileRepresentable {
+        let profilesOrGroups: [ProfileRepresentable] = sourceId >= 0 ? profiles : groups // source_id — ID of the news source (positive number signifies a user; negative number signifies a community).
+        let normalSourseId = sourceId >= 0 ? sourceId : -sourceId // Module value (to avoid negative id)
+        let profileRepresentable = profilesOrGroups.first { (myProfileRepresentable) -> Bool in
+            myProfileRepresentable.id == normalSourseId
+        }
+        return profileRepresentable! // fix IT!
     }
 }
